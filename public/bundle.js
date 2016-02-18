2a17c42747f24ad2fc6cb22043fb01d1acc75f14
Damon Kalaboukis <damonkala@yahoo.com>
2016-02-18 14:39:22 -0800

    Heroku needs cors apparently

--- public/bundle.js
+++ public/bundle.js
@@ -4,8 +4,8 @@ var app = angular.module('gameCompare', ['ui.router', 'angular-jwt', 'ngCookies'
 
 
 app.constant('ENV', {
-  // API_URL: 'https://game-compare.herokuapp.com'
-  API_URL: 'http://localhost:3000'
+  API_URL: 'https://game-compare.herokuapp.com'
+  // API_URL: 'http://localhost:3000'
 });
 
 
@@ -414,49 +414,6 @@ angular.module('gameCompare')
 
 angular.module('gameCompare')
 
-
-.controller('registerCtrl', function($scope, $state, UserService){
-	console.log("LODADED");
-	$scope.submit = function(user){
-		if(user.password !== user.password2){
-			swal({
-				type: "warning",
-				title: "Passwords don't match!",
-				text: "Matching passwords only please",
-				showConfirmButton: true,
-				confirmButtonText: "Gotcha.",
-			});
-			return;
-		}
-		if(!user.email){
-			swal({
-				type: "error",
-				title: "Give us your email address!",
-				text: "C'mon, we know that's a fake!",
-				showConfirmButton: true,
-				confirmButtonText: "I hear ya.",
-			});
-			return;
-		}
-		UserService.register(user)
-
-		.then(function(data){
-			swal({
-				type: "success",
-				title: "Successful registration!",
-				text: "Hurray. You're a User!",
-				imageUrl: "images/thumbs-up.jpg"
-			});			$state.go('login');
-		}, function(err){
-			console.log(err);
-		});
-	}
-});
-
-'use strict';
-
-angular.module('gameCompare')
-
 .controller('searchCtrl', function($scope, $http, ENV, GameService){
 	$scope.loading = false;
 	console.log("LOADING?", $scope.loading);
@@ -542,6 +499,49 @@ angular.module('gameCompare')
 		};
 	}
 ]);
+
+'use strict';
+
+angular.module('gameCompare')
+
+
+.controller('registerCtrl', function($scope, $state, UserService){
+	console.log("LODADED");
+	$scope.submit = function(user){
+		if(user.password !== user.password2){
+			swal({
+				type: "warning",
+				title: "Passwords don't match!",
+				text: "Matching passwords only please",
+				showConfirmButton: true,
+				confirmButtonText: "Gotcha.",
+			});
+			return;
+		}
+		if(!user.email){
+			swal({
+				type: "error",
+				title: "Give us your email address!",
+				text: "C'mon, we know that's a fake!",
+				showConfirmButton: true,
+				confirmButtonText: "I hear ya.",
+			});
+			return;
+		}
+		UserService.register(user)
+
+		.then(function(data){
+			swal({
+				type: "success",
+				title: "Successful registration!",
+				text: "Hurray. You're a User!",
+				imageUrl: "images/thumbs-up.jpg"
+			});			$state.go('login');
+		}, function(err){
+			console.log(err);
+		});
+	}
+});
 
 'use strict';
 
