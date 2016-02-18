0f113a5088eebb0d9d12f12509af3a857c98e0e9
Damon Kalaboukis <damonkala@yahoo.com>
2016-02-18 14:44:28 -0800

    Making reviews look good, and getting the goddamn image to show

--- public/bundle.js
+++ public/bundle.js
@@ -414,6 +414,49 @@ angular.module('gameCompare')
 
 angular.module('gameCompare')
 
+
+.controller('registerCtrl', function($scope, $state, UserService){
+	console.log("LODADED");
+	$scope.submit = function(user){
+		if(user.password !== user.password2){
+			swal({
+				type: "warning",
+				title: "Passwords don't match!",
+				text: "Matching passwords only please",
+				showConfirmButton: true,
+				confirmButtonText: "Gotcha.",
+			});
+			return;
+		}
+		if(!user.email){
+			swal({
+				type: "error",
+				title: "Give us your email address!",
+				text: "C'mon, we know that's a fake!",
+				showConfirmButton: true,
+				confirmButtonText: "I hear ya.",
+			});
+			return;
+		}
+		UserService.register(user)
+
+		.then(function(data){
+			swal({
+				type: "success",
+				title: "Successful registration!",
+				text: "Hurray. You're a User!",
+				imageUrl: "images/thumbs-up.jpg"
+			});			$state.go('login');
+		}, function(err){
+			console.log(err);
+		});
+	}
+});
+
+'use strict';
+
+angular.module('gameCompare')
+
 .controller('searchCtrl', function($scope, $http, ENV, GameService){
 	$scope.loading = false;
 	console.log("LOADING?", $scope.loading);
@@ -499,49 +542,6 @@ angular.module('gameCompare')
 		};
 	}
 ]);
-
-'use strict';
-
-angular.module('gameCompare')
-
-
-.controller('registerCtrl', function($scope, $state, UserService){
-	console.log("LODADED");
-	$scope.submit = function(user){
-		if(user.password !== user.password2){
-			swal({
-				type: "warning",
-				title: "Passwords don't match!",
-				text: "Matching passwords only please",
-				showConfirmButton: true,
-				confirmButtonText: "Gotcha.",
-			});
-			return;
-		}
-		if(!user.email){
-			swal({
-				type: "error",
-				title: "Give us your email address!",
-				text: "C'mon, we know that's a fake!",
-				showConfirmButton: true,
-				confirmButtonText: "I hear ya.",
-			});
-			return;
-		}
-		UserService.register(user)
-
-		.then(function(data){
-			swal({
-				type: "success",
-				title: "Successful registration!",
-				text: "Hurray. You're a User!",
-				imageUrl: "images/thumbs-up.jpg"
-			});			$state.go('login');
-		}, function(err){
-			console.log(err);
-		});
-	}
-});
 
 'use strict';
 
