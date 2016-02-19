09dfdc33d41665fb1af3911cc52a0f2e5b3272a6
Damon Kalaboukis <damonkala@yahoo.com>
2016-02-18 17:19:08 -0800

    is.. is it working?

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('build', ['clean'], function(){
 		.pipe(concat("bundle.js"))
 		.pipe(addsrc("source/**/*.html"))
     .pipe(addsrc("./source/**/*.css"))
-    // .pipe(addsrc("./source/styles/*.ttf"))
+    .pipe(addsrc("./source/styles/*.ttf"))
 		.pipe(gulp.dest('public'))
 	gulp.src('assets/**/*')
 		.pipe(gulp.dest('public/assets'))
