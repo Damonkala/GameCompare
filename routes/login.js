0d464f34b94790ea5610c7b7465158f2b9489e8d
Damon Kalaboukis <damonkala@yahoo.com>
2016-02-20 13:40:54 -0800

    I think I broke everything for a sec

--- routes/login.js
+++ routes/login.js
@@ -5,19 +5,17 @@ var jwt = require('jwt-simple')
 
 
 router.post('/', function(req, res){
-  console.log("Made it to the route");
-  console.log("3: IS THERE A USER", req.body);
-  // User.login(req.body, function(err, user){
-  //   if(user){
-  //     console.log("4: IS THERE A USER", user);
-  //     var token = jwt.encode(user, process.env.JWT_SECRET);
-  //
-  //     console.log(token)
-  //     res.cookie('token', token).send('login succesfull')
-  //   } else{
-  //     res.send('Incorrect Username or Password!')
-  //   }
-  // })
+  User.login(req.body, function(err, user){
+    if(user){
+      console.log("4: IS THERE A USER", user);
+      var token = jwt.encode(user, process.env.JWT_SECRET);
+
+      console.log(token)
+      res.cookie('token', token).send('login succesfull')
+    } else{
+      res.send('Incorrect Username or Password!')
+    }
+  })
 })
 
 
