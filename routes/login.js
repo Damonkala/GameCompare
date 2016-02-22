72c91edd49e19deeba2059daa2bc769e0c0f6578
Damon Kalaboukis <damonkala@yahoo.com>
2016-02-22 15:24:08 -0800

    login route reset

--- routes/login.js
+++ routes/login.js
@@ -4,15 +4,10 @@ var User = require('../models/User')
 var jwt = require('jwt-simple')
 
 router.post('/', function(req, res){
-  console.log("Made it to the route");
-  console.log("3: IS THERE A USER", req.body);
   User.login(req.body, function(err, user){
     if(user){
-      console.log("4: IS THERE A USER", user);
-      console.log("THAT's THE WAY WE WASH OUR HANDS!");
-      console.log("NOT ALL ENDS WELL");
       var token = jwt.encode(user, process.env.JWT_SECRET);
-      // console.log("TORKEN!", token)
+      console.log(token)
       res.cookie('token', token).send('login succesfull')
     } else{
       res.send('Incorrect Username or Password!')
