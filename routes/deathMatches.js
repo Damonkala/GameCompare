3fb8821d01ce9dc595240f9095646be777a21730
Damon Kalaboukis <damonkala@yahoo.com>
2016-03-22 17:20:23 -0700

    Users cannot vote for their own review

--- routes/deathMatches.js
+++ routes/deathMatches.js
@@ -43,36 +43,42 @@ router.get('/:id', function(req, res){
 	})
 })
 router.put('/upvote', function(req, res){
-	User.find({$and: [{_id: req.body.userInfo}, {votes: req.body.deathMatch}] }, function(err, userReview){
+	User.find({$and: [{_id: req.body.userInfo}, {deathMatches: req.body.deathMatch}] }, function(err, userReview){
 		if (err || userReview[0]){return console.log(err) || console.log("ALREADY VOTED");}
-		else{
-			console.log("Here we are");
-			UserReview.findByIdAndUpdate(req.body.review, { $inc: {score: 1}}, function(err, userReview){
-				console.log(userReview);
-				User.findByIdAndUpdate(req.body.criticId, { $inc: {score: 1}}, function(err, user) {
-					console.log(user);
-					User.findByIdAndUpdate(req.body.userInfo,  {$push: {votes: req.body.deathMatch}}, function(err, user){
+		User.find({$and: [{_id: req.body.userInfo}, {votes: req.body.deathMatch}] }, function(err, userReview){
+			if (err || userReview[0]){return console.log(err) || console.log("ALREADY VOTED");}
+			else{
+				console.log("Here we are");
+				UserReview.findByIdAndUpdate(req.body.review, { $inc: {score: 1}}, function(err, userReview){
+					console.log(userReview);
+					User.findByIdAndUpdate(req.body.criticId, { $inc: {score: 1}}, function(err, user) {
 						console.log(user);
+						User.findByIdAndUpdate(req.body.userInfo,  {$push: {votes: req.body.deathMatch}}, function(err, user){
+							console.log(user);
+						})
 					})
 				})
-			})
-		}
+			}
+		})
 	})
 })
 router.put('/downvote', function(req, res){
-	User.find({$and: [{_id: req.body.userInfo}, {votes: req.body.deathMatch}] }, function(err, userReview){
-		if (err || userReview[0]){return console.log(err) || res.send("voted");}
-		else{
-			UserReview.findByIdAndUpdate(req.body.review, { $inc: {score: -1}}, function(err, userReview){
-				console.log(userReview);
-				User.findByIdAndUpdate(req.body.criticId, { $inc: {score: -1}}, function(err, user) {
-					console.log(user);
-					User.findByIdAndUpdate(req.body.userInfo,  {$pull: {votes: req.body.deathMatch}}, function(err, user){
+	User.find({$and: [{_id: req.body.userInfo}, {deathMatches: req.body.deathMatch}] }, function(err, userReview){
+		if (err || userReview[0]){return console.log(err) || console.log("ALREADY VOTED");}
+		User.find({$and: [{_id: req.body.userInfo}, {votes: req.body.deathMatch}] }, function(err, userReview){
+			if (err || userReview[0]){return console.log(err) || res.send("voted");}
+			else{
+				UserReview.findByIdAndUpdate(req.body.review, { $inc: {score: -1}}, function(err, userReview){
+					console.log(userReview);
+					User.findByIdAndUpdate(req.body.criticId, { $inc: {score: -1}}, function(err, user) {
 						console.log(user);
+						User.findByIdAndUpdate(req.body.userInfo,  {$pull: {votes: req.body.deathMatch}}, function(err, user){
+							console.log(user);
+						})
 					})
 				})
-			})
-		}
+			}
+		})
 	})
 })
 router.put('/:id', function(req, res){
