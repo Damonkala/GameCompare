82b533038ca2c47ff3ca00f5a654e9bc9e55181c
Damon Kalaboukis <damonkala@yahoo.com>
2016-03-28 16:17:32 -0700

    Bcrypt

--- public/bundle.js
+++ public/bundle.js
@@ -488,6 +488,35 @@ app.service('UserService', function($http, $location, $rootScope, $cookies, jwtH
 'use strict';
 
 angular.module('gameCompare')
+.controller('loginCtrl', function($scope, $state, $rootScope, UserService, jwtHelper, $cookies){
+	$scope.submit = function(user){
+		UserService.login(user)
+		.then(function(res){
+			$scope.$emit('loggedIn');
+			if(res.data === "Incorrect Username or Password!"){
+				swal({
+					type: "error",
+					title: "Uh-Oh!",
+					text: res.data,
+					showConfirmButton: true,
+					confirmButtonText: "I hear ya.",
+				});
+			} else{
+				document.cookie = 'token' + "=" + res.data;
+				var token = $cookies.get('token');
+				var decoded = jwtHelper.decodeToken(token);
+				UserService.loggedIn = 'true';
+				$state.go('userPage', {"username": user.username})
+			}
+		}, function(err) {
+			console.error(err);
+		});
+	}
+});
+
+'use strict';
+
+angular.module('gameCompare')
 
 
 .controller('registerCtrl', function($scope, $state, UserService){
@@ -523,35 +552,6 @@ angular.module('gameCompare')
 			});			$state.go('login');
 		}, function(err){
 			console.log(err);
-		});
-	}
-});
-
-'use strict';
-
-angular.module('gameCompare')
-.controller('loginCtrl', function($scope, $state, $rootScope, UserService, jwtHelper, $cookies){
-	$scope.submit = function(user){
-		UserService.login(user)
-		.then(function(res){
-			$scope.$emit('loggedIn');
-			if(res.data === "Incorrect Username or Password!"){
-				swal({
-					type: "error",
-					title: "Uh-Oh!",
-					text: res.data,
-					showConfirmButton: true,
-					confirmButtonText: "I hear ya.",
-				});
-			} else{
-				document.cookie = 'token' + "=" + res.data;
-				var token = $cookies.get('token');
-				var decoded = jwtHelper.decodeToken(token);
-				UserService.loggedIn = 'true';
-				$state.go('userPage', {"username": user.username})
-			}
-		}, function(err) {
-			console.error(err);
 		});
 	}
 });
