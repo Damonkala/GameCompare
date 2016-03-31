81984d232adf4e8f5e2c2f82d5525cc1d799808d
Damon Kalaboukis <damonkala@yahoo.com>
2016-03-30 22:27:18 -0700

    Users now gain score from they're score

--- public/bundle.js
+++ public/bundle.js
@@ -180,69 +180,6 @@ app.service('GameService', function($http, $location, $rootScope, $cookies, jwtH
 
 angular.module('gameCompare')
 
-.controller('homeCtrl', function($scope, $http){
-	$http.get(`/games/`).then( function victory(resp) {
-		$scope.dbGames = resp.data;
-	}, function failure(err) {
-		console.log(err);
-	});
-})
-
-'use strict';
-
-var app = angular.module('gameCompare');
-
-app.service('ScopeMaster', function($http, $location, $rootScope, $cookies, jwtHelper){
-	this.setScopes = function(data){
-		var trueGame = data;
-		var noReview = {criticScore: 0, userScore: 0, url:null}
-		trueGame.cover = data.cover ? data.cover[0].url : '//res.cloudinary.com/igdb/image/upload/t_thumb/nocover_qhhlj6.jpg';
-
-		trueGame.gameSpotCriticScore = data.gamespot.length ? data.gamespot[0].criticScore : 0
-		trueGame.gameSpotUserScore = data.gamespot.length ? data.gamespot[0].userScore : 0
-		trueGame.gameSpotUrl = data.gamespot.length ? data.gamespot[0].url : undefined
-
-		trueGame.gamesRadarCriticScore = data.gamesradar.length ? data.gamesradar[0].criticScore : 0
-		trueGame.gamesRadarUserScore = data.gamesradar.length ? data.gamesradar[0].userScore : 0
-		trueGame.gamesRadarUrl = data.gamesradar.length ? data.gamesradar[0].url : undefined
-
-		trueGame.metacriticCriticScore = data.metacritic.length ? data.metacritic[0].criticScore : 0
-		trueGame.metacriticUserScore = data.metacritic.length ? data.metacritic[0].userScore : 0
-		trueGame.metacriticUrl = data.metacritic.length ? data.metacritic[0].url : undefined
-
-		trueGame.ignCriticScore = data.ign.length ? data.ign[0].criticScore : 0
-		trueGame.ignUserScore = data.ign.length ? data.ign[0].userScore : 0
-		trueGame.ignUrl = data.ign.length ? data.ign[0].url : undefined
-
-		var criticScore = [trueGame.gameSpotCriticScore, trueGame.gamesRadarCriticScore, trueGame.ignCriticScore, trueGame.metacriticCriticScore]
-		var userScore = [trueGame.gameSpotUserScore, trueGame.gamesRadarUserScore, trueGame.ignUserScore, trueGame.metacriticUserScore]
-
-		function totalScore(scores) {
-			var total = [];
-
-			for(var i = 0; i<scores.length;i++){
-				if(!Number(scores[i])){
-					total.push(0)
-				} else {
-					total.push(Number(scores[i]))
-				}
-			}
-			return total.reduce(function(a, b){
-				return a + b;
-
-			})
-		}
-		trueGame.totalCritic = totalScore(criticScore)
-		trueGame.totalUser = totalScore(userScore)
-
-		return trueGame
-	}
-})
-
-'use strict';
-
-angular.module('gameCompare')
-
 .controller('listCtrl', function($scope, $http, $state, GameService, $timeout){
 	$scope.loading = false;
 	var loadingPics = ["http://www.contemporary-home-computing.org/idioms/wp-content/uploads/mario.gif", "http://vignette3.wikia.nocookie.net/kirby/images/7/70/Sonic_1_Running.gif/revision/latest?cb=20140909010956&path-prefix=en", "http://rs128.pbsrc.com/albums/p195/R3DG3CKO/pacman.gif~c200", "https://49.media.tumblr.com/e818add8c7f18bf8c6e45d61ec83d89a/tumblr_ms85ibKsgO1rf4po9o1_250.gif"]
@@ -368,6 +305,69 @@ angular.module('gameCompare')
 		restrict: 'AE',
 		templateUrl: "views/search-view.html"
 	}
+})
+
+'use strict';
+
+var app = angular.module('gameCompare');
+
+app.service('ScopeMaster', function($http, $location, $rootScope, $cookies, jwtHelper){
+	this.setScopes = function(data){
+		var trueGame = data;
+		var noReview = {criticScore: 0, userScore: 0, url:null}
+		trueGame.cover = data.cover ? data.cover[0].url : '//res.cloudinary.com/igdb/image/upload/t_thumb/nocover_qhhlj6.jpg';
+
+		trueGame.gameSpotCriticScore = data.gamespot.length ? data.gamespot[0].criticScore : 0
+		trueGame.gameSpotUserScore = data.gamespot.length ? data.gamespot[0].userScore : 0
+		trueGame.gameSpotUrl = data.gamespot.length ? data.gamespot[0].url : undefined
+
+		trueGame.gamesRadarCriticScore = data.gamesradar.length ? data.gamesradar[0].criticScore : 0
+		trueGame.gamesRadarUserScore = data.gamesradar.length ? data.gamesradar[0].userScore : 0
+		trueGame.gamesRadarUrl = data.gamesradar.length ? data.gamesradar[0].url : undefined
+
+		trueGame.metacriticCriticScore = data.metacritic.length ? data.metacritic[0].criticScore : 0
+		trueGame.metacriticUserScore = data.metacritic.length ? data.metacritic[0].userScore : 0
+		trueGame.metacriticUrl = data.metacritic.length ? data.metacritic[0].url : undefined
+
+		trueGame.ignCriticScore = data.ign.length ? data.ign[0].criticScore : 0
+		trueGame.ignUserScore = data.ign.length ? data.ign[0].userScore : 0
+		trueGame.ignUrl = data.ign.length ? data.ign[0].url : undefined
+
+		var criticScore = [trueGame.gameSpotCriticScore, trueGame.gamesRadarCriticScore, trueGame.ignCriticScore, trueGame.metacriticCriticScore]
+		var userScore = [trueGame.gameSpotUserScore, trueGame.gamesRadarUserScore, trueGame.ignUserScore, trueGame.metacriticUserScore]
+
+		function totalScore(scores) {
+			var total = [];
+
+			for(var i = 0; i<scores.length;i++){
+				if(!Number(scores[i])){
+					total.push(0)
+				} else {
+					total.push(Number(scores[i]))
+				}
+			}
+			return total.reduce(function(a, b){
+				return a + b;
+
+			})
+		}
+		trueGame.totalCritic = totalScore(criticScore)
+		trueGame.totalUser = totalScore(userScore)
+
+		return trueGame
+	}
+})
+
+'use strict';
+
+angular.module('gameCompare')
+
+.controller('homeCtrl', function($scope, $http){
+	$http.get(`/games/`).then( function victory(resp) {
+		$scope.dbGames = resp.data;
+	}, function failure(err) {
+		console.log(err);
+	});
 })
 
 'use strict';
--- routes/userReviews.js
+++ routes/userReviews.js
@@ -35,27 +35,78 @@ router.put('/vote', function(req, res) {
 				if (userReview){
 					User.findByIdAndUpdate(req.body.currentUserId, {$pull: {votes: oldVote[0]}},
 						function(err, user){
-							res.send('GRATS')
-						})
-					} else {
-						console.log("Something else then");
-					}
-				})
-				// }
-			} else {
-				UserReview.findByIdAndUpdate(req.body.reviewVotedForId, {$inc: {score: req.body.voteValue}},
-					function(err, userReview){
-						if (userReview){
-							User.findByIdAndUpdate(req.body.currentUserId, {$push: {votes: vote}},
-								function(err, user){
-									res.send('GRATS')
+							if(user){
+								User.findByIdAndUpdate(req.body.authorVotedForId, { $inc: {score: revert}},
+									function(err, user){
+										if(user){
+											res.send('GRATS')
+										}
+									})
+								}
+							})
+						} else {
+							console.log("Something else then");
+						}
+					})
+					// }
+				} else {
+					UserReview.findByIdAndUpdate(req.body.reviewVotedForId, {$inc: {score: req.body.voteValue}},
+						function(err, userReview){
+							if (userReview){
+								User.findByIdAndUpdate(req.body.currentUserId, {$push: {votes: vote}},
+									function(err, user){
+										if(user){
+											User.findByIdAndUpdate(req.body.authorVotedForId, { $inc: {score: req.body.voteValue}},
+												function(err, user){
+													if(user){
+														res.send('GRATS')
+													}
+												})
+											}
+										})
+									} else {
+										console.log("Something else then");
+									}
 								})
-							} else {
-								console.log("Something else then");
 							}
 						})
-					}
-				})
-			})
-			
-			module.exports = router;
+					})
+					// router.post('/hasVoted', function(req, res){
+					// 	User.find({$and: [{_id: req.body.userInfo}, {votes: req.body.deathMatch}]})
+					// })
+					// router.put('/upvote', function(req, res){
+					// 	User.find({$and: [{_id: req.body.userInfo}, {deathMatches: req.body.deathMatch}] }, function(err, userReview){
+					// 		if (err || userReview[0]){return console.log(err) || console.log("ALREADY VOTED");}
+					// 		User.find({$and: [{_id: req.body.userInfo}, {votes: req.body.deathMatch}] }, function(err, userReview){
+					// 			if (err || userReview[0]){return console.log(err) || console.log("ALREADY VOTED");}
+					// 			else{
+					// UserReview.findByIdAndUpdate(req.body.review, { $inc: {score: 1}}, function(err, userReview){
+					// 					User.findByIdAndUpdate(req.body.criticId, { $inc: {score: 1}}, function(err, user) {
+					// User.findByIdAndUpdate(req.body.userInfo,  {$push: {votes: req.body.deathMatch}}, function(err, user){
+					// 							res.status(err ? 400 : 200).send(err || user)
+					// 						})
+					// 					})
+					// 				})
+					// 			}
+					// 		})
+					// 	})
+					// })
+					// router.put('/downvote', function(req, res){
+					// 	User.find({$and: [{_id: req.body.userInfo}, {deathMatches: req.body.deathMatch}] }, function(err, userReview){
+					// 		if (err || userReview[0]){return console.log(err) || console.log("ALREADY VOTED");}
+					// 		User.find({$and: [{_id: req.body.userInfo}, {votes: req.body.deathMatch}] }, function(err, userReview){
+					// 			if (err || userReview[0]){return console.log(err) || res.send("voted");}
+					// 			else{
+					// 				UserReview.findByIdAndUpdate(req.body.review, { $inc: {score: -1}}, function(err, userReview){
+					// 					User.findByIdAndUpdate(req.body.criticId, { $inc: {score: -1}}, function(err, user) {
+					// 						User.findByIdAndUpdate(req.body.userInfo,  {$pull: {votes: req.body.deathMatch}}, function(err, user){
+					// 							res.status(err ? 400 : 200).send(err || user)
+					// 						})
+					// 					})
+					// 				})
+					// 			}
+					// 		})
+					// 	})
+					// })
+
+					module.exports = router;
